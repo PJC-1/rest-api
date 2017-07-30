c6b2e6afa1ab36793c4bcb040f04dd83715b83ac
PJC-1 <jumpzine@gmail.com>
2017-07-29 23:39:17 -0700

    added some error handling middleware

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api',require('./routes/api'));
 
+// error handling middleware
+app.use(function(err, req, res, next){
+    // console.log(err);
+    res.status(422).send({error: err.message});
+});
+
 // listen for requests
 // will listen for the port specified by the environment variable (heroku, etc.)
 app.listen(process.env.port || 4000, function(){
