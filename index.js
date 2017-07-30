97447e30b8dbfd6b072d15352f2f51bb5a7dbac7
PJC-1 <jumpzine@gmail.com>
2017-07-29 23:27:20 -0700

    connecting to the mongodb

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 // set up express app
 const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+// overriding mongoose Promise with the global object's Promise
+mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 // initialize routes
